6a10909775f77d3c7afaf9ddcc925b98cc004212
Marcel Tunnissen <marcelteun@gmail.com>
2018-05-13 13:44:21 +0200

    Start using NPM/webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './show-off.js',
+  output: {
+    filename: 'show-off-bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production'
+};
+
