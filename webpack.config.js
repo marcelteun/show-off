69de9a54fa4eee784d12a4e0a3b1a376884dade2
Marcel Tunnissen <marcelteun@gmail.com>
2019-03-07 22:04:38 +0100

    Install and use terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './show-off.js',
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
   output: {
     filename: 'show-off-bundle.js',
     path: path.resolve(__dirname, 'dist')
