fcbc55f6096ad9f78a9054f53222c0dd08e8668a
Marcel Tünnissen <marceltn@axis.com>
2020-07-19 10:41:13 +0200

    webpack-config: add minimize field for quick debugging
    Change-Id: I642b29b60246fb5fca7866774811d72d2227187f

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   entry: './show-off.js',
   optimization: {
+    minimize: true,
     minimizer: [new TerserPlugin()],
   },
   output: {
